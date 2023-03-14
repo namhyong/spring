aa6b01b73cc8800a8819a8f66db4ea1a05f72464
namhyong <namhyong2@gmail.com>
2023-03-14 19:04:29 +0900

    feat:"0314"

---
+++ first/src/main/java/sesac/sesac_spring/SesacSpringApplication.java
@@ -0,0 +1,13 @@
+package sesac.sesac_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesacSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SesacSpringApplication.class, args);
+	}
+
+}
---
+++ first/src/test/java/sesac/sesac_spring/SesacSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sesac.sesac_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SesacSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
